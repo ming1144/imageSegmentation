f5a20cb4ef095e1c22e048cb9b1ba13855d15f22
ming1144 <https://github.com/ming1144>
2017-03-10 17:05:32 +0800

    20170310 Mac

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@
 using namespace std;
 
 int maskHeight = 33;
-bool padding = false;
+bool padding = true;
 
 int main(int argc, char *argv[])
 {
@@ -41,10 +41,7 @@ int main(int argc, char *argv[])
 	mkdir("1", 0777);
 	chdir("../");
 
-	
-	
-	string testImageFolder = "trainingData/male/";
-
+	string testImageFolder = "trainingData/all/";
 
 	string testfile_filename = testImageFolder;
 	testfile_filename += "origin/testfile.txt";
@@ -74,20 +71,88 @@ int main(int argc, char *argv[])
 		
 		QImage imgGroundTruth(QString::fromStdString(groundTruth));
 		QImage imgTest(QString::fromStdString(test));
+		QImage imgTestGray(imgTest.width(), imgTest.height(), QImage::Format_Grayscale8);
+		imgTestGray = imgTest.convertToFormat(QImage::Format_Grayscale8);
 
-		int start = maskHeight/2;
+		uchar* p_imgGroundTruth, *p_imgTestGray, *p_ROI;
+
+		int start = maskHeight / 2;
 		if (padding)
 		{
 			start = 0;
 		}
-		for (int i = start; i < imgTest.height() - start; i++)
+
+		p_imgGroundTruth = imgGroundTruth.bits();
+		p_imgTestGray = imgTestGray.bits();
+
+
+		for (int i = start; i < imgTest.height() - start; i++, p_imgGroundTruth += imgGroundTruth.bytesPerLine(), p_imgTestGray += imgTestGray.bytesPerLine())
 		{
 			for (int j = start; j < imgTest.width() - start; j++)
 			{
-				QColor clrCurrent(imgGroundTruth.pixel(j, i));
+				bool groundTruthIsRed;
+				if (p_imgGroundTruth[j * 4 + 2] == 255)
+				{
+					groundTruthIsRed = true;
+				}
+				else
+				{
+					groundTruthIsRed = false;
+				}
+
+				QImage ROI(maskHeight, maskHeight,QImage::Format_Grayscale8);
+				ROI.fill(QColor(0, 0, 0));
+				uchar* temp_p_imgTestGray;
+				int x_start, x_end, y_start, y_end;
+
+				if (i < maskHeight / 2)
+				{
+					y_start = maskHeight / 2 - i;
+				}
+				else
+				{
+					y_start = 0;
+				}
+
+				if (i > imgGroundTruth.height() - maskHeight / 2)
+				{
+					y_end = maskHeight - imgGroundTruth.height() - i;
+				}
+				else
+				{
+					y_end = maskHeight;
+				}
+
+				if (j < maskHeight / 2)
+				{
+					x_start = maskHeight / 2 - j;
+				}
+				else
+				{
+					x_start = 0;
+				}
+
+				if (j > imgGroundTruth.width() - maskHeight / 2)
+				{
+					x_end = maskHeight - imgGroundTruth.width() - j;
+				}
+				else
+				{
+					x_end = maskHeight;
+				}
+
+				for (int y = y_start; y < y_end; y++)
+				{
+					p_ROI = ROI.scanLine(y);
+					temp_p_imgTestGray = imgTestGray.scanLine(i + (y - maskHeight / 2));
+					for (int x = x_start; x < x_end; x++)
+					{
+						p_ROI[x] = temp_p_imgTestGray[j + (x - maskHeight / 2)];
+					}
+				}
+
 				string filename;
-				QRect rect(QPoint(j - maskHeight/2, i - maskHeight/2), QPoint(j + maskHeight/2, i + maskHeight/2));
-				if (clrCurrent.red() == 255 && clrCurrent.green() == 0 && clrCurrent.blue() == 0)
+				if (groundTruthIsRed)
 				{
 					filename = True;
 				}
@@ -95,19 +160,17 @@ int main(int argc, char *argv[])
 				{
 					filename = False;
 				}
+
 				filename += testfileList[m].c_str();
 				filename += "_";
 				filename += to_string(i);
 				filename += "_";
 				filename += to_string(j);
 				filename += ".PNG";
-				QImage copy = imgTest.copy(rect);
-				copy.save(QString::fromStdString(filename));
+				ROI.save(QString::fromStdString(filename));
 			}
 		}
-		
 	}
-	
-	//return a.exec();
+
 	return 0;
 }
